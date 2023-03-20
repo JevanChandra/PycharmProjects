9eaf87e368f23102564d1bfb597e81f5ac85cec1
Jevan <jevan.chandra@my.jcu.edu.au>
2023-03-20 15:27:00 +0800

    Initial commit

--- cp1404practicals/prac_02/password_stars.py
+++ cp1404practicals/prac_02/password_stars.py
@@ -1,10 +1,21 @@
-password = input("Enter password: ")
+def main():
+    password = get_password()
+    print_password(password)
 
-while len(password) < 5:
-    print("Your password does not reach the minimum length. "
-          "Please try again")
-    password = input("Enter password: ")
-else:
+
+def print_password(password):
     for i in range(0, len(password)):
         print("*", end='')
+
+
+def get_password():
+    password = input("Enter password: ")
+    while len(password) < 5:
+        print("Your password does not reach the minimum length. "
+              "Please try again")
+        password = input("Enter password: ")
+    return password
+
+
+main()
 
