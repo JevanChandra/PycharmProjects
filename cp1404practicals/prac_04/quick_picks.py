44a882b240975699450f5901f4548088a7d51220
Jevan <jevan.chandra@my.jcu.edu.au>
2023-04-05 11:48:27 +0800

    Initial commit

--- cp1404practicals/prac_04/quick_picks.py
+++ cp1404practicals/prac_04/quick_picks.py
@@ -4,7 +4,7 @@ quick_picks = input("How many quick picks? ")
 
 for i in range(0, int(quick_picks)):
     numbers = []
-    for j in range(0, int(quick_picks)):
+    for j in range(0, 6):
         number = random.randint(1, 45)
         if number in numbers:
             number = random.randint(number + 1, 45)
