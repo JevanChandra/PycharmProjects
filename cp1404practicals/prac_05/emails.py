3ceff40db3176ded99e810a0af45fbfa89568d74
Jevan <jevan.chandra@my.jcu.edu.au>
2023-04-15 01:26:52 +0800

    Complete emails.py exercise

---
+++ cp1404practicals/prac_05/emails.py
@@ -0,0 +1,21 @@
+"""
+Emails
+
+Estimate: 30 minutes
+Actual  : 30 minutes
+"""
+
+email_dictionary = {}
+email = input("Email: ")
+
+while email != "":
+    name = " ".join(email.split("@")[0].split(".")).title()
+    choice = input(f"Is your name {name}? (Y/n) ").lower()
+    if choice != "" and choice != "y":
+        name = input("Name: ").title()
+    email_dictionary[name] = email
+    email = input("Email: ")
+
+for email in email_dictionary.items():
+    print(email)
+    print(f"{email[0]} ({email[1]})")
