d77a3308767bc278363490c7b5dd3cf571bfd68f
Jevan <jevan.chandra@my.jcu.edu.au>
2023-04-14 13:41:42 +0800

    Complete state_name.py exercise

--- cp1404practicals/prac_05/state_name.py
+++ cp1404practicals/prac_05/state_name.py
@@ -2,6 +2,9 @@
 CP1404/CP5632 Practical
 State names in a dictionary
 File needs reformatting
+
+Estimate: 10 minutes
+Actual: 13 minutes
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
