03a53a4accb8b38d5fa5f17fe773497f524e4f48
Jevan <jevan.chandra@my.jcu.edu.au>
2023-04-25 23:08:20 +0800

    Completed menu section and added functions.

--- cp1404practicals/A1/travel_tracker.py
+++ cp1404practicals/A1/travel_tracker.py
@@ -13,9 +13,8 @@ def main():
     user_menu_choice = input(">>> ").upper()
     while user_menu_choice != "Q" and user_menu_choice != "0":
         if user_menu_choice == "L" or user_menu_choice == "1":
-            places = out_file.readline()
-            print(places)
-            for i in places:
+            for i in out_file:
+                places = i.strip().split(',')
                 print(places)
             print("L")
         elif user_menu_choice == "R" or user_menu_choice == "2":
@@ -32,6 +31,7 @@ def main():
 
 
 def count_amount_of_places(in_file, total):
+    """Counts the amount of places there are in the file"""
     for i in in_file:
         total += 1
     return total
